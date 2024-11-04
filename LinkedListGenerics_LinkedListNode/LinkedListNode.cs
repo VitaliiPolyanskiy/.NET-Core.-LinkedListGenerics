d6831117c629d604138353ab638e655483e40e8d
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-11-04 16:50:07 +0200

    Add project files.

---
+++ LinkedListGenerics_LinkedListNode/LinkedListNode.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создадим связный список
+            LinkedList<string> link = new LinkedList<string>();
+
+            // Добавим несколько элементов
+            link.AddFirst("Украина");
+            link.AddFirst("Австрия");
+            link.AddFirst("Швейцария");
+            link.AddFirst("Франция");
+
+            // Отобразим элементы в прямом направлении
+            LinkedListNode<string> node;
+            Console.WriteLine("Элементы коллекции в прямом направлении: ");
+            for (node = link.First; node != null; node = node.Next)
+                Console.WriteLine(node.Value);
+
+            // Отобразить элементы в обратном направлении
+            Console.WriteLine("\n\nЭлементы коллекции в обратном направлении: ");
+            for (node = link.Last; node != null; node = node.Previous)
+                Console.WriteLine(node.Value);
+
+            Console.ReadLine();
+        }
+    }
+}
